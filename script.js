52810d2919f90b406a42259619ebcc37421995be
democracy111 <96644402+democracy111@users.noreply.github.com>
2021-12-25 15:21:24 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showpopup () {
+  var Popup = document.getElementById('popup')
+  Popup.classList.add('show')
+  /* document.location.href = "https://secure.donus.org/wwfkorea/pay/step1"  ;   */  
+}
+
+function hidepopup () {
+  var Popup = document.getElementById('popup')
+  Popup.classList.remove('show')  
+}
